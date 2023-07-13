c6fc00620cc33e03ac74b0821ab1b034b3ae67e5
Noxyangel <forgotrrr@gmail.com>
2023-07-13 16:18:00 +0300

    add mui palettes

--- src/components/layout/muiThemeProvider.tsx
+++ src/components/layout/muiThemeProvider.tsx
@@ -8,12 +8,32 @@ import { useTheme } from "next-themes";
 const darkTheme = createTheme({
     palette: {
         mode: "dark",
+        primary: {
+            main: "#846CEF",
+            light: "#B2A3F5",
+            dark: "#140849",
+        },
+        secondary: {
+            main: "#18DCDC",
+            light: "#A3F5F5",
+            dark: "#084949",
+        },
     },
 });
 
 const lightTheme = createTheme({
     palette: {
         mode: "light",
+        primary: {
+            main: "#4723E7",
+            light: "#B2A3F5",
+            dark: "#140849",
+        },
+        secondary: {
+            main: "#18DCDC",
+            light: "#A3F5F5",
+            dark: "#084949",
+        },
     },
 });
 
